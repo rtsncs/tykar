72c444149034a933c4db2c53c4dd8f1fc949f66f
Dominik Brzeziński <scnstr@proton.me>
2024-09-25 21:12:38 +0200

    login modal

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -3,48 +3,28 @@ import {
   chakra,
   Flex,
   Heading,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
   Spacer,
   useDisclosure,
 } from "@chakra-ui/react";
+import Login from "./login";
 
 function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isLoginOpen,
+    onOpen: onLoginOpen,
+    onClose: onLoginClose,
+  } = useDisclosure();
+
   return (
     <>
       <chakra.header borderBottom="thin solid gray" boxShadow="md">
         <Flex p={8}>
           <Heading>Tykar</Heading>
           <Spacer />
-          <Button onClick={onOpen}>Login</Button>
+          <Button onClick={onLoginOpen}>Login</Button>
         </Flex>
+        <Login isOpen={isLoginOpen} onClose={onLoginClose}></Login>
       </chakra.header>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Login</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody display="flex" flexDir="column" gap={4}>
-            <Input placeholder="Login" type="text" />
-            <Input placeholder="Password" type="password" />
-          </ModalBody>
-
-          <ModalFooter>
-            <Button variant="ghost" onClick={onClose} mr={3}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue">Login</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
     </>
   );
 }
--- front/src/components/login.tsx
+++ front/src/components/login.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,27 +8,65 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Spinner,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import { DefaultApi } from "../api";
 
-function Login(isOpen: boolean, onClose: () => void) {
+function Login(props: { isOpen: boolean; onClose: () => void }) {
+  const { isOpen, onClose } = props;
+
+  const [username, setName] = useState("");
+  const handleNameChange = (e) => setName(e.target.value);
+
+  const [password, setPassword] = useState("");
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const [requestSend, setRequestSend] = useState(false);
+
+  const api = new DefaultApi();
+
+  function login() {
+    if (requestSend || !username || !password) return;
+    setRequestSend(true);
+    api.login({ username, password }).then(() => {
+      setRequestSend(false);
+    });
+    onClose();
+  }
+
   return (
-    <>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Login</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>cock</ModalBody>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Login</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody display="flex" flexDir="column" alignItems="center" gap={4}>
+          {requestSend && <Spinner size="lg" />}
+          <Input
+            placeholder="Login"
+            type="text"
+            value={username}
+            onChange={handleNameChange}
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </ModalBody>
 
-          <ModalFooter>
-            <Button variant="ghost" onClick={onClose} mr={3}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue">Login</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
+        <ModalFooter>
+          <Button variant="ghost" onClick={onClose} mr={3}>
+            Cancel
+          </Button>
+          <Button colorScheme="blue" onClick={login}>
+            Login
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 }
 
